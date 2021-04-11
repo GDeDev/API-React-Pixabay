6265fc3b0d86fa7f513795b05343cea65f60c122
Giuliano Damico <giulianodamico2019@gmail.com>
2021-04-11 17:18:23 -0300

    App build

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     <div className="container mx-auto">
       <ImageSearch searchText={(text) => setTerm(text)} />
 
-      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No hay imagenes</h1> }
 
       {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Cargando...</h1> : <div className="grid grid-cols-3 gap-4">
         {images.map(image => (
